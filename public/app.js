c7e55ae796d7b8f9e6358b2cb0740aba2f63209d
Pablo Hernandez <hpablo575@gmail.com>
2022-09-11 11:42:32 -0600

    third

---
+++ public/app.js
@@ -0,0 +1,17 @@
+doucument.addEventListener('DOMContentLoaded', event => {
+
+    const app = firebase.app();
+    console.log(app)
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    
+    .then(result => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user)
+    });
+}
