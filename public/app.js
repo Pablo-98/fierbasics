d0ad62ba826674c2da456c7880e0349952886f6f
Pablo Hernandez <hpablo575@gmail.com>
2022-09-21 10:33:55 -0600

    added a function that updates data from the app to the database

--- public/app.js
+++ public/app.js
@@ -6,11 +6,17 @@ document.addEventListener("DOMContentLoaded", event => {
 
     const myPost = db.collection('posts').doc('firstPost');
 
-    myPost.get()
-    .then(doc => {
+    myPost.onSnapshot(doc => {
 
         const data = doc.data();
         document.write( data.title + `<br>`)
-        document.write( data.createdAt )
+        document.write( data.createdAt + `<br>` )
     })
+    
 });
+
+function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstPost');
+    myPost.update({ title: e.target.value })
+}
